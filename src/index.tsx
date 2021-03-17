c3ad08475e9b0e56fad182e9156f8fd988affdcf
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-17 17:01:39 +0800

    Executing using scrDoc

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,12 @@ const App = () => {
 		setCode(result.outputFiles[0].text);
 	};
 
+	const html = `
+		<script>
+			${code}
+		</script>
+	`;
+
 	return (
 		<div>
 			<textarea onChange={(e) => setInput(e.target.value)} />
@@ -45,6 +51,7 @@ const App = () => {
 				<button onClick={onClick}>Submit</button>
 			</div>
 			<pre>{code}</pre>
+			<iframe sandbox="allow-scripts" srcDoc={html} title="App" />
 		</div>
 	);
 };
