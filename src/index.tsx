17c100b9f9f8df8236bf71bcbd9ecc3a1ea93518
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-17 11:06:51 +0800

    Basic form inplementationn

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	return <h1>App</h1>;
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const onClick = () => {
+		console.log(input);
+	};
+
+	return (
+		<div>
+			<textarea onChange={(e) => setInput(e.target.value)} />
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
